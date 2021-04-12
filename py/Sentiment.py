7d205e9a985c8566d1a432751bc7125fccdc70c7
LiangRongjia <liangrongjia999@foxmail.com>
2021-04-12 15:55:29 +0800

    改一下 py

---
+++ py/Sentiment.py
@@ -0,0 +1,46 @@
+from keras import Sequential
+from keras.layers import Bidirectional,LSTM,Dense,Embedding,Dropout,Activation,Softmax
+
+# 封装模型和方法
+class Sentiment:
+    def __init__(self, w2id, embedding_weights, Embedding_dim, maxlen, labels_category):
+        self.Embedding_dim = Embedding_dim
+        self.embedding_weights = embedding_weights
+        self.vocab = w2id
+        self.labels_category = labels_category
+        self.maxlen = maxlen
+        self.model = self.build_model()
+
+    def build_model(self):
+        model = Sequential()
+        # input dim(140,100)
+        model.add(Embedding(output_dim=self.Embedding_dim,
+                            input_dim=len(self.vocab) + 1,
+                            weights=[self.embedding_weights],
+                            input_length=self.maxlen))
+        model.add(Bidirectional(LSTM(50), merge_mode='concat'))
+        model.add(Dropout(0.5))
+        model.add(Dense(self.labels_category))
+        model.add(Activation('softmax'))
+        model.compile(loss='categorical_crossentropy',
+                      optimizer='adam',
+                      metrics=['accuracy'])
+        model.summary()
+        return model
+
+    def train(self, X_train, y_train, X_test, y_test, n_epoch=5):
+        self.model.fit(X_train, y_train, batch_size=32, epochs=n_epoch,
+                       validation_data=(X_test, y_test))
+        self.model.save('sentiment.h5')
+
+    def predict(self, model_path, new_sen):
+        model = self.model
+        model.load_weights(model_path)
+        new_sen_list = jieba.lcut(new_sen)
+        sen2id = [self.vocab.get(word, 0) for word in new_sen_list]
+        sen_input = pad_sequences([sen2id], maxlen=self.maxlen)
+        res = model.predict(sen_input)[0]
+        return np.argmax(res)
+
+    def load_model(self,modelPath):
+        self.model.load_weights(modelPath)
--- py/calc.py
+++ py/calc.py
@@ -1,123 +1,43 @@
 import json
 import os
 
-devMode = 'false'
+# 生产/开发模式
+devMode = True
 
-# 生产模式使用下面路径
-if devMode == 'false':
-    path_dataToPy = os.getcwd() + '/resources/app/py/dataToPy.json'
-    path_dataToNodeJs = os.getcwd() + '/resources/app/py/dataToNodeJs.json'
-# 开发模式使用下面路径
+# 生产/模式使用不同路径
+if not devMode:
+    pyDir = os.getcwd() + '/resources/app/py/'
 else:
-    path_dataToPy = os.getcwd() + '/py/dataToPy.json'
-    path_dataToNodeJs = os.getcwd() + '/py/dataToNodeJs.json'
+    pyDir = os.getcwd() + '/py/'
+path_dataToPy = pyDir + 'dataToPy.json'
+path_dataToNodeJs = pyDir + 'dataToNodeJs.json'
 
-f1 = open(path_dataToPy, 'r', encoding="utf-8")
-data = json.loads(f1.read())
-f1.close()
-if 'comment' in data:
-    f = open(path_dataToNodeJs, 'w', encoding="utf-8")
-    f.write('{ "theDimension": "评论角度", "theAttitude": "评论态度", "theTextFeatures": "文字特征", "theReply": "自动回复" }')  
-    f.close()
-elif 'data' in data:
-    f = open(path_dataToNodeJs, 'w', encoding="utf-8")
-    f.write(
-        """
-            {
-                "data": [
-                    {
-                        "commentText": "item",
-                        "dimension": "外观",
-                        "attitude": "负",
-                        "textFeatures": "文字特征",
-                        "reply": "自动回复"
-                    },
-                    {
-                        "commentText": "item",
-                        "dimension": "性能",
-                        "attitude": "正",
-                        "textFeatures": "文字特征",
-                        "reply": "自动回复"
-                    },
-                    {
-                        "commentText": "item",
-                        "dimension": "性能",
-                        "attitude": "正",
-                        "textFeatures": "文字特征",
-                        "reply": "自动回复"
-                    },
-                    {
-                        "commentText": "item",
-                        "dimension": "手感",
-                        "attitude": "负",
-                        "textFeatures": "文字特征",
-                        "reply": "自动回复"
-                    },
-                    {
-                        "commentText": "item",
-                        "dimension": "服务",
-                        "attitude": "正",
-                        "textFeatures": "文字特征",
-                        "reply": "自动回复"
-                    },
-                    {
-                        "commentText": "item",
-                        "dimension": "价格",
-                        "attitude": "正",
-                        "textFeatures": "文字特征",
-                        "reply": "自动回复"
-                    },
-                    {
-                        "commentText": "item",
-                        "dimension": "价格",
-                        "attitude": "正",
-                        "textFeatures": "文字特征",
-                        "reply": "自动回复"
-                    },
-                    {
-                        "commentText": "item",
-                        "dimension": "外观",
-                        "attitude": "正",
-                        "textFeatures": "文字特征",
-                        "reply": "自动回复"
-                    },
-                    {
-                        "commentText": "item",
-                        "dimension": "外观",
-                        "attitude": "正",
-                        "textFeatures": "文字特征",
-                        "reply": "自动回复"
-                    },
-                    {
-                        "commentText": "item",
-                        "dimension": "外观",
-                        "attitude": "正",
-                        "textFeatures": "文字特征",
-                        "reply": "自动回复"
-                    },
-                    {
-                        "commentText": "item",
-                        "dimension": "外观",
-                        "attitude": "正",
-                        "textFeatures": "文字特征",
-                        "reply": "自动回复"
-                    },
-                    {
-                        "commentText": "item",
-                        "dimension": "外观",
-                        "attitude": "正",
-                        "textFeatures": "文字特征",
-                        "reply": "自动回复"
-                    },
-                    {
-                        "commentText": "item",
-                        "dimension": "外观",
-                        "attitude": "正",
-                        "textFeatures": "文字特征",
-                        "reply": "自动回复"
-                    }
-                ]
-            }
-        """
-    )  
-    f.close()
+# 读取输入，输入字符串放在 data 里
+f_input = open(path_dataToPy, 'r', encoding="utf-8")
+data = json.loads(f_input.read())
+f_input.close()
+
+if 'comment' in data:  # 单条分析，评论通过 json.loads(f.read()).comment 获得
+    # 应用模型，输出放在 output
+    # ...
+    output = '{ "theDimension": "评论角度", "theAttitude": "评论态度", "theTextFeatures": "文字特征", "theReply": "自动回复" }'
+elif 'data' in data:  # 批量分析，评论数组通过 json.loads(f.read()).data 获得
+    # 应用模型，输出放在 output
+    # ...
+    output = """{"data": [
+        {"commentText": "item","dimension": "外观","attitude": "负","textFeatures": "文字特征","reply": "自动回复"},
+        {"commentText": "item","dimension": "性能","attitude": "正","textFeatures": "文字特征","reply": "自动回复"},
+        {"commentText": "item","dimension": "外观","attitude": "负","textFeatures": "文字特征","reply": "自动回复"},
+        {"commentText": "item","dimension": "性能","attitude": "正","textFeatures": "文字特征","reply": "自动回复"},
+        {"commentText": "item","dimension": "手感","attitude": "负","textFeatures": "文字特征","reply": "自动回复"},
+        {"commentText": "item","dimension": "性能","attitude": "正","textFeatures": "文字特征","reply": "自动回复"},
+        {"commentText": "item","dimension": "外观","attitude": "正","textFeatures": "文字特征","reply": "自动回复"},
+        {"commentText": "item","dimension": "性能","attitude": "正","textFeatures": "文字特征","reply": "自动回复"},
+        {"commentText": "item","dimension": "外观","attitude": "正","textFeatures": "文字特征","reply": "自动回复"},
+        {"commentText": "item","dimension": "性能","attitude": "正","textFeatures": "文字特征","reply": "自动回复"}
+    ]}"""
+
+# 输出到文件
+f_output = open(path_dataToNodeJs, 'w', encoding="utf-8")
+f_output.write(output)
+f_output.close()
